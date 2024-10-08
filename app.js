7d4b8feb29624516a43a03776ec1e06d6ee5d2ba
phat-dat-ag <67agphatdat@gmail.com>
2024-10-08 19:34:09 +0700

    Dinh nghia cac route cho tai nguyen Reader

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 
+const readersRouter = require("./app/routes/reader.route");
+
 const app = express();
 
 app.use(cors());
@@ -9,5 +11,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application." });
 });
+
+app.use("/api/readers", readersRouter);
 
 module.exports = app;
