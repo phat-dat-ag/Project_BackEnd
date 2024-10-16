ebae93b9a6e9f8bfaaddec8118637a394880ee34
phat-dat-ag <67agphatdat@gmail.com>
2024-10-16 20:53:42 +0700

    Hau nhu hoan thanh CSDL cho Reader

--- app/controllers/reader.controller.js
+++ app/controllers/reader.controller.js
@@ -1,27 +1,130 @@
-exports.create = (req, res) => {
-    res.send({ message: "create handler" });
+const ReaderService = require("../services/reader.service");
+const MongoDB = require("../utils/mongodb.util");
+const ApiError = require("../api-error");
+
+exports.create = async (req, res, next) => {
+    // Phải nhập last_name
+    if (!req.body?.last_name) {
+        return next(new ApiError(400, "last_name can not be empty"));
+    }
+    try {
+        const readerService = new ReaderService(MongoDB.client);
+        // Gọi để lưu đối tượng reader xuống CSDL
+        const document = await readerService.create(req.body);
+        return res.send(document);
+    } catch (error) {
+        // Coi lỗi gì
+        console.log(error);
+        return next(
+            new ApiError(500, "An error occurred while creating the reader")
+        );
+    }
 };
 
-exports.findAll = (req, res) => {
-    res.send({ message: "findAll handler" });
+exports.findAll = async (req, res, next) => {
+    let documents = [];
+
+    try {
+        const readerService = new ReaderService(MongoDB.client);
+        // Tìm theo tên last_name
+        const { last_name } = req.query;
+        if (last_name) {
+            documents = await readerService.findByLastName(last_name);
+        } else {
+            documents = await readerService.find({});
+        }
+    } catch (error) {
+        // Coi lỗi gì
+        console.log(error);
+        return next(
+            new ApiError(500, "An error occurred while creating the reader")
+        );
+    }
+    // Trả về danh sách tìm được
+    return res.send(documents);
 };
 
-exports.findOne = (req, res) => {
-    res.send({ message: "findOne handler" });
+// Tìm 1 reader theo id_reader
+exports.findOne = async (req, res, next) => {
+    try {
+        const readerService = new ReaderService(MongoDB.client);
+        const document = await readerService.findByIdReader(req.params.id);
+        if (!document) {
+            return next(new ApiError(404, "Reader not found"));
+        }
+        return res.send(document);
+    } catch (error) {
+        // Coi lỗi gì
+        console.log(error);
+        return next(
+            new ApiError(500, `Error retrieving reader with id=${req.params.id}`)
+        );
+    }
 };
 
-exports.update = (req, res) => {
-    res.send({ message: "update handler" });
+exports.update = async (req, res, next) => {
+    if (Object.keys(req.body).lenth === 0) {
+        return next(new ApiError(400, "Data to update can not be empty"));
+    }
+    try {
+        const readerService = new ReaderService(MongoDB.client);
+        const document = await readerService.update(req.params.id, req.body);
+        if (!document) {
+            return next(new ApiError(404, "Reader not found"));
+        }
+        return res.send({ message: "Reader was updated successfully" });
+    } catch (error) {
+        // Coi lỗi gì
+        console.log(error);
+        return next(
+            new ApiError(500, `Error updating reader with id=${req.params.id}`)
+        );
+    }
 };
 
-exports.delete = (req, res) => {
-    res.send({ message: "delete handler" });
+exports.delete = async (req, res, next) => {
+    try {
+        const readerService = new ReaderService(MongoDB.client);
+        const document = await readerService.delete(req.params.id);
+        if (!document) {
+            return next(new ApiError(404, "Reader not found"));
+        }
+        return res.send({ message: "Reader was delete successfully" });
+    } catch (error) {
+        // Coi lỗi gì
+        console.log(error);
+        return next(
+            new ApiError(500, `Could not delete reader with id=${req.params.id}`)
+        );
+    }
 };
 
-exports.deleteAll = (req, res) => {
-    res.send({ message: "deleteAll handler" });
+exports.deleteAll = async (req, res, next) => {
+    try {
+        const readerService = new ReaderService(MongoDB.client);
+        const deletedCount = await readerService.deleteAll();
+        return res.send({
+            message: `${deletedCount} readers were deleted successfully`,
+        });
+    } catch (error) {
+        // Coi lỗi gì
+        console.log(error);
+        return next(
+            new ApiError(500, "An error occurred while removing all readers")
+        );
+    }
 };
 
-exports.findAllFavorite = (req, res) => {
-    res.send({ message: "findAllFavorite handler" });
+exports.findAllFavorite = async (req, res, next) => {
+    try {
+        const readerService = new ReaderService(MongoDB.client);
+        const documents = await readerService.findFavorite();
+        return res.send(documents);
+    } catch (error) {
+        // Coi lỗi gì
+        console.log(error);
+        return next(
+            new ApiError(500, "An error occurred while retrieving favorite readers")
+        );
+    }
 };
---
+++ app/services/reader.service.js
@@ -0,0 +1,84 @@
+const { ObjectId, ReturnDocument } = require("mongodb");
+
+class ReaderService {
+    constructor(client) {
+        this.Reader = client.db().collection("readers");
+    }
+    // Định nghĩa các phương thức truy xuất CSDL sử dụng mongodb API
+
+    // Hỗ trợ cho phương thức create()
+    extractReaderData(payload) {
+        const reader = {
+            first_name: payload.first_name,
+            last_name: payload.last_name,
+            birthday: payload.birthday,
+            sex: payload.sex,
+            address: payload.address,
+            phone: payload.phone,
+            favorite: payload.favorite,
+        };
+        // Xóa các trường không xác định
+        Object.keys(reader).forEach(
+            (key) => reader[key] === undefined && delete reader[key]
+        );
+        return reader;
+    };
+
+    async create(payload) {
+        const reader = this.extractReaderData(payload);
+        const result = await this.Reader.findOneAndUpdate(
+            reader,
+            { $set: { favorite: reader.favorite === true } },
+            { returnDocument: "after", upsert: true }
+        );
+        return result;
+    }
+
+    async find(filter) {
+        const cursor = await this.Reader.find(filter);
+        return await cursor.toArray();
+    }
+
+    async findByLastName(last_name) {
+        return await this.find({
+            name: { $regex: new RegExp(new RegExp(last_name)), $options: "i" },
+        });
+    }
+
+    async findByIdReader(id) {
+        return await this.Reader.findOne({
+            _id: ObjectId.isValid(id) ? new ObjectId(id) : null,
+        })
+    }
+
+    async update(id, payload) {
+        const filter = {
+            _id: ObjectId.isValid(id) ? new ObjectId(id) : null,
+        };
+        const update = this.extractReaderData(payload);
+        const result = await this.Reader.findOneAndUpdate(
+            filter,
+            { $set: update },
+            { returnDocument: "after" }
+        );
+        return result;
+    }
+
+    async delete(id) {
+        const result = await this.Reader.findOneAndDelete({
+            _id: ObjectId.isValid(id) ? new ObjectId(id) : null,
+        });
+        return result;
+    }
+
+    async findFavorite() {
+        return await this.find({ favorite: true });
+    }
+
+    async deleteAll() {
+        const result = await this.Reader.deleteMany({});
+        return result.deletedCount;
+    }
+}
+
+module.exports = ReaderService;
